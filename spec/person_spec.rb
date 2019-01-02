26c80e0f4429aa26d6e19f0d29babcb60d1cea3f
Nicolas Hennick <nicolashennick@gmail.com>
2019-01-02 08:57:53 +0000

    initial commit

---
+++ spec/person_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/person"
+
+describe Person do
+  describe "#full_name" do
+    it "returns the first and last names concatenated" do
+      # Write your spec here.
+      # You can delete the pending line.
+      pending "Not written yet"
+    end
+  end
+end
